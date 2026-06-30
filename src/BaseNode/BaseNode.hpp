c6ddf727f7189d9c03c02c76145942ab5d3c560b
David Gitz <davidgitz@gmail.com>
2026-06-30 05:58:18 -0500

    WIP

---
+++ src/BaseNode/BaseNode.hpp
@@ -0,0 +1,14 @@
+/**
+ * @file BaseNode.hpp
+ * @author your name (you@domain.com)
+ * @brief
+ * @version 0.1
+ * @date 2026-06-30
+ *
+ * @copyright Copyright (c) 2026
+ *
+ */
+#pragma once
+namespace fast::rf::robot_framework_ros {
+class BaseNode {};
+}  // namespace fast::rf::robot_framework_ros
