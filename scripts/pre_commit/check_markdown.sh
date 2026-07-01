d1465736ee3a3890361282630b38840178085020
David Gitz <davidgitz@gmail.com>
2026-07-01 07:19:53 -0500

    WIP

--- scripts/pre_commit/check_markdown.sh
+++ scripts/pre_commit/check_markdown.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Checking Markdown Links..."
-find . -name "*.md" -print0 | xargs -0 lychee --exclude-path "templates/*"
+find . -name "*.md" | xargs lychee --exclude-path "templates/*"
 if [ $? -ne 0 ]; then
     echo "❌ Error: Markdown Link Checking failed. Aborting commit."
     exit 1
