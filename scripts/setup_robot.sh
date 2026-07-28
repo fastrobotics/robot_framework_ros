eec247a6a4f02d41e580498b86c32f62f4c5a3a8
David Gitz <davidgitz@gmail.com>
2026-07-28 18:24:07 -0500

    Work complete.

--- scripts/setup_robot.sh
+++ scripts/setup_robot.sh
@@ -1,3 +1,3 @@
 echo "Setup Robot"
 sudo apt update
-sudo apt install -y build-essential cmake gcovr libgeographic-dev ros-noetic-tf2
+sudo apt install -y build-essential cmake gcovr libgeographic-dev ros-noetic-tf2 ros-noetic-tf2-geometry-msgs
