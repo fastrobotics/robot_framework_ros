c5815eccdb754c2473b82315347c0f0e1ac9fce3
David Gitz <davidgitz@gmail.com>
2026-07-28 22:56:45 -0500

    Updates

--- scripts/setup_ide.sh
+++ scripts/setup_ide.sh
@@ -2,7 +2,7 @@ echo "Setting up IDE"
 sudo apt update
 sudo apt install -y default-jre graphviz
 pip install --upgrade pip
-pip install pre-commit cmakelang
+pip install pre-commit cmakelang bagpy
 pipx install cmake-format
 pushd ~/Downloads
 wget https://github.com/plantuml/plantuml/releases/download/v1.2026.6/plantuml-1.2026.6.jar
