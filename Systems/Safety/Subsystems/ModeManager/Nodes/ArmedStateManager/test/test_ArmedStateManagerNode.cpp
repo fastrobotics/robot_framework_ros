77c829557a2806e907e787e83d34e7cc4c14e2a3
David Gitz <davidgitz@gmail.com>
2026-08-15 14:51:42 -0500

    Fixed tests.

--- Systems/Safety/Subsystems/ModeManager/Nodes/ArmedStateManager/test/test_ArmedStateManagerNode.cpp
+++ Systems/Safety/Subsystems/ModeManager/Nodes/ArmedStateManager/test/test_ArmedStateManagerNode.cpp
@@ -26,6 +26,7 @@ std::string arm_command_topic = "/test/arm_command";
 std::string ready_to_arm_process1_topic = "/test/nodeBasicTeleopControl/ready_to_arm";
 std::string ready_to_arm_process2_topic = "/test/nodeServoHat/ready_to_arm";
 std::string ready_to_arm_process3_topic = "/test/nodeTankDriveExecutor/ready_to_arm";
+std::string ready_to_arm_process4_topic = "/test/nodeTrajectoryController/ready_to_arm";
 std::string armstate_change_service_topic = "/test/arm_state_change";
 void heartbeat_Callback([[maybe_unused]] const robot_framework_ros::heartbeat& msg) { heartbeat_rx_count++; }
 void diagnostic_Callback([[maybe_unused]] const robot_framework_ros::diagnostic& msg) { diagnostic_rx_count++; }
@@ -50,6 +51,9 @@ TEST(ArmedStateManagerNode, TestBasics) {
     ros::Publisher ready_to_arm_process3_pub =
         nh.advertise<robot_framework_ros::ready_to_arm>(ready_to_arm_process3_topic, 1);
 
+    ros::Publisher ready_to_arm_process4_pub =
+        nh.advertise<robot_framework_ros::ready_to_arm>(ready_to_arm_process4_topic, 1);
+
     ros::Subscriber arm_command_sub = nh.subscribe(arm_command_topic, 100, &arm_command_Callback);
 
     sleep(5.0);
@@ -63,6 +67,7 @@ TEST(ArmedStateManagerNode, TestBasics) {
     EXPECT_EQ(1, ready_to_arm_process1_pub.getNumSubscribers());
     EXPECT_EQ(1, ready_to_arm_process2_pub.getNumSubscribers());
     EXPECT_EQ(1, ready_to_arm_process3_pub.getNumSubscribers());
+    EXPECT_EQ(1, ready_to_arm_process4_pub.getNumSubscribers());
 
     sleep(1.0);  // Wait for ArmedStateManagerNode to Start.
     EXPECT_TRUE(heartbeat_rx_count > 0);
@@ -87,6 +92,12 @@ TEST(ArmedStateManagerNode, TestBasics) {
     process3_data.ProcessID = fast::rf::UserInterfaceSystem::RemoteControlSubsystem::PROCESS_TELEOPCONTROL_ID;
     process3_data.ready_to_arm = true;
 
+    robot_framework_ros::ready_to_arm process4_data;
+    process4_data.SystemID = fast::rf::NavigationSystem::SYSTEM_ID;
+    process4_data.SubsystemID = fast::rf::NavigationSystem::NavigationExecutorSubsystem::SUBSYSTEM_ID;
+    process4_data.ProcessID = fast::rf::NavigationSystem::NavigationExecutorSubsystem::PROCESS_TRAJECTORY_CONTROLLER_ID;
+    process4_data.ready_to_arm = true;
+
     // Feed it Ready To Arm Topics
     double current_time = 0.0;
     double delta_t = 0.1;
@@ -94,6 +105,7 @@ TEST(ArmedStateManagerNode, TestBasics) {
         ready_to_arm_process1_pub.publish(process1_data);
         ready_to_arm_process2_pub.publish(process2_data);
         ready_to_arm_process3_pub.publish(process3_data);
+        ready_to_arm_process4_pub.publish(process4_data);
         current_time += delta_t;
         usleep(delta_t * 1000000.0);
         if (arm_command.armed_state.state == (uint8_t)fast::rf::ArmedState::DISARMED) {
@@ -115,6 +127,7 @@ TEST(ArmedStateManagerNode, TestBasics) {
         ready_to_arm_process1_pub.publish(process1_data);
         ready_to_arm_process2_pub.publish(process2_data);
         ready_to_arm_process3_pub.publish(process3_data);
+        ready_to_arm_process4_pub.publish(process4_data);
         current_time += delta_t;
         usleep(delta_t * 1000000.0);
         if (arm_command.armed_state.state == (uint8_t)fast::rf::ArmedState::ARMED) {
@@ -131,6 +144,7 @@ TEST(ArmedStateManagerNode, TestBasics) {
         ready_to_arm_process1_pub.publish(process1_data);
         ready_to_arm_process2_pub.publish(process2_data);
         ready_to_arm_process3_pub.publish(process3_data);
+        ready_to_arm_process4_pub.publish(process4_data);
         current_time += delta_t;
         usleep(delta_t * 1000000.0);
         if (arm_command.armed_state.state == (uint8_t)fast::rf::ArmedState::DISARMED) {
